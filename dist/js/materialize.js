713099e49cc4a24834fcd072640cce662e436981
Ando Emerencia <ando.emerencia@gmail.com>
2020-08-06 05:44:44 +0200

    recompile again

--- dist/js/materialize.js
+++ dist/js/materialize.js
@@ -1225,23 +1225,6 @@ M.escapeHash = function (hash) {
   return hash.replace(/(:|\.|\[|\]|,|=|\/)/g, '\\$1');
 };
 
-/**
- * Get closest ancestor that satisfies the condition
- * @param {Element} el  Element to find ancestors on
- * @param {Function} condition  Function that given an ancestor element returns true or false
- * @returns {Element} Return closest ancestor or null if none satisfies the condition
- */
-M.getClosestAncestor = function (el, condition) {
-  var ancestor = el.parentNode;
-  while (ancestor !== null && !$(ancestor).is(document)) {
-    if (condition(ancestor)) {
-      return ancestor;
-    }
-    ancestor = ancestor.parentNode;
-  }
-  return null;
-};
-
 M.elementOrParentIsFixed = function (element) {
   var $element = $(element);
   var $checkElements = $element.add($element.parents());
@@ -2307,7 +2290,11 @@ $jscomp.polyfill = function (e, r, p, m) {
       _this9.filterQuery = [];
 
       // Move dropdown-content after dropdown-trigger
-      _this9._moveDropdown();
+      if (!!_this9.options.container) {
+        $(_this9.options.container).append(_this9.dropdownEl);
+      } else {
+        _this9.$el.after(_this9.dropdownEl);
+      }
 
       _this9._makeDropdownFocusable();
       _this9._resetFilterQueryBound = _this9._resetFilterQuery.bind(_this9);
@@ -2577,20 +2564,6 @@ $jscomp.polyfill = function (e, r, p, m) {
           opacity: ''
         });
       }
-
-      // Move dropdown after container or trigger
-
-    }, {
-      key: "_moveDropdown",
-      value: function _moveDropdown(containerEl) {
-        if (!!this.options.container) {
-          $(this.options.container).append(this.dropdownEl);
-        } else if (containerEl) {
-          $(containerEl).append(this.dropdownEl);
-        } else {
-          this.$el.after(this.dropdownEl);
-        }
-      }
     }, {
       key: "_makeDropdownFocusable",
       value: function _makeDropdownFocusable() {
@@ -2613,7 +2586,7 @@ $jscomp.polyfill = function (e, r, p, m) {
       }
     }, {
       key: "_getDropdownPosition",
-      value: function _getDropdownPosition(closestOverflowParent) {
+      value: function _getDropdownPosition() {
         var offsetParentBRect = this.el.offsetParent.getBoundingClientRect();
         var triggerBRect = this.el.getBoundingClientRect();
         var dropdownBRect = this.dropdownEl.getBoundingClientRect();
@@ -2629,6 +2602,9 @@ $jscomp.polyfill = function (e, r, p, m) {
           height: idealHeight,
           width: idealWidth
         };
+
+        // Countainer here will be closest ancestor with overflow: hidden
+        var closestOverflowParent = !!this.dropdownEl.offsetParent ? this.dropdownEl.offsetParent : this.dropdownEl.parentNode;
 
         var alignments = M.checkPossibleAlignments(this.el, closestOverflowParent, dropdownBounds, this.options.coverTrigger ? 0 : triggerBRect.height);
 
@@ -2646,11 +2622,10 @@ $jscomp.polyfill = function (e, r, p, m) {
             this.isScrollable = true;
 
             // Determine which side has most space and cutoff at correct height
-            idealHeight -= 20; // Add padding when cutoff
             if (alignments.spaceOnTop > alignments.spaceOnBottom) {
               verticalAlignment = 'bottom';
               idealHeight += alignments.spaceOnTop;
-              idealYPos -= alignments.spaceOnTop - 20; // add back padding space
+              idealYPos -= alignments.spaceOnTop; // add back padding space
             } else {
               idealHeight += alignments.spaceOnBottom;
             }
@@ -2762,23 +2737,11 @@ $jscomp.polyfill = function (e, r, p, m) {
     }, {
       key: "_placeDropdown",
       value: function _placeDropdown() {
-        // Countainer here will be closest ancestor with overflow: hidden
-        var closestOverflowParent = M.getClosestAncestor(this.dropdownEl, function (ancestor) {
-          return $(ancestor).css('overflow') !== 'visible';
-        });
-        // Fallback
-        if (!closestOverflowParent) {
-          closestOverflowParent = !!this.dropdownEl.offsetParent ? this.dropdownEl.offsetParent : this.dropdownEl.parentNode;
-        }
-        if ($(closestOverflowParent).css('position') === 'static') $(closestOverflowParent).css('position', 'relative');
-
-        this._moveDropdown(closestOverflowParent);
-
         // Set width before calculating positionInfo
         var idealWidth = this.options.constrainWidth ? this.el.getBoundingClientRect().width : this.dropdownEl.getBoundingClientRect().width;
         this.dropdownEl.style.width = idealWidth + 'px';
 
-        var positionInfo = this._getDropdownPosition(closestOverflowParent);
+        var positionInfo = this._getDropdownPosition();
         this.dropdownEl.style.left = positionInfo.x + 'px';
         this.dropdownEl.style.top = positionInfo.y + 'px';
         this.dropdownEl.style.height = positionInfo.height + 'px';
